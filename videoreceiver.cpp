42bcbfdb206af1ece7dfe138805523a0536b0923
Kaustuv Pokharel <kastuvpokharel@gmail.com>
2024-01-27 12:57:39 -0500

    working on some headers, and class methods

--- videoreceiver.cpp
+++ videoreceiver.cpp
@@ -1,3 +1,14 @@
 #include "videoreceiver.h"
 
-VideoReceiver::VideoReceiver() {}
+VideoReceiver::VideoReceiver(QObject *parent) : QObject(parent) , socket(nullptr), expectedDatasize(0)
+{
+    socket = new QTcpSocket(this);
+    connect(socket, &QTcpSocket::readyRead, this, &VideoReceiver::readData);
+}
+
+void VideoReceiver::connectToServer(const QString &serverAddress, quint16 port)
+{
+    socket->connectToHost(serverAddress, port);
+}
+
+
--- videoreceiver.h
+++ videoreceiver.h
@@ -2,12 +2,32 @@
 #define VIDEORECEIVER_H
 
 #include <QObject>
+#include <QTcpSocket>
+#include <QImage>
 
 class VideoReceiver : public QObject
 {
     Q_OBJECT
 public:
-    VideoReceiver();
+    explicit VideoReceiver(QObject *parent = nullptr);
+
+signals:
+    void frameReceived(const QImage &frame);
+
+public slots:
+    void connectToServer(const QString &serverAddress, quint16 port);
+    void disconnectFromServer();
+
+private slots:
+    void readData();
+
+private:
+    QTcpSocket *socket;
+    quint64 expectedDatasize;
+    QByteArray buffer;
+
+    QImage byteArrayToImage(const QByteArray &data);
+
 };
 
 #endif // VIDEORECEIVER_H
