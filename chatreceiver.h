0dd8a45b5a1c93a920b8716dd627c97a59d5c48e
Kaustuv Pokharel <kastuvpokharel@gmail.com>
2024-01-27 19:02:04 -0500

    Initiated text chat socket, to bind in the frontend

---
+++ chatreceiver.h
@@ -0,0 +1,13 @@
+#ifndef CHATRECEIVER_H
+#define CHATRECEIVER_H
+
+#include <QObject>
+
+class chatreceiver : public QObject
+{
+    Q_OBJECT
+public:
+    chatreceiver();
+};
+
+#endif // CHATRECEIVER_H
